c7e728c63fb2a9331991221570b17be26c466e47
BELEMMoumouni <90107382+BELEMMoumouni@users.noreply.github.com>
2022-11-30 00:57:30 +0100

    Add files via upload

---
+++ src/Entree.cpp
@@ -0,0 +1,26 @@
+//
+// Created by utilisateur on 09/11/2021.
+//
+
+
+#include "Entree.h"
+#include <istream>
+void Entree::afficher(){
+    cout << nom;
+    cout << " ";
+    cout << numero;
+    cout << "  ";
+}
+
+Entree::Entree(string name, string num){
+    this->nom = name;
+    this->numero = num;
+}
+
+ostream  & operator<<(ostream& out , Entree&  entree) {
+    out << "Nom : " << entree.nom << " Telephone : " << entree.numero << endl;
+    return out;
+}
+bool operator/(string & nom, Entree & entree) {
+    return (entree.nom == nom);
+}
